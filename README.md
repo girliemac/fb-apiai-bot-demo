74351ca57702273eeca9faf835cbe2625e1e0c7f
Tomomi ❤ Imura <tomomi.i@gmail.com>
2017-01-03 14:41:17 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Facebook Chat Bot Demo
+
+This demo shows the manual integration with [Facebook Messenger](https://developers.facebook.com/),
+[API.ai](https://console.api.ai), and [Open Weather Map API](http://openweathermap.org/).
+
+This bot allows you to have a small talk on Facebook Messenger.
+If you ask about weather, the bot returns with the current condition in a certain city.
+
+![Facebook scan code](https://github.com/girliemac/fb-apiai-bot-demo/blob/master/public/images/fb-bot.gif)
+
+## Try the Facebook Bot
+
+Scan this code with your FB Messenger mobile app, and start a conversation.
+
+![Facebook scan code](https://github.com/girliemac/fb-apiai-bot-demo/blob/master/public/images/messenger-scan-code.png)
---
+++ webhook.js
@@ -0,0 +1,141 @@
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiai = require('apiai');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const apiaiApp = apiai(APIAI_TOKEN);
+
+/* For Facebook Validation */
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
+
+/* Handling all messenges */
+app.post('/webhook', function (req, res) {
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach(function (entry) {
+      entry.messaging.forEach(function (event) {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
+
+// function sendMessage(event) {
+//   let sender = event.sender.id;
+//   let text = event.message.text;
+//
+//   console.log('*** RECEIVED ***');
+//   console.log(event);
+//
+//   request({
+//     url: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: {access_token: PAGE_ACCESS_TOKEN},
+//     method: 'POST',
+//     json: {
+//       recipient: {id: sender},
+//       message: {text: text}
+//     }
+//   }, function (error, response) {
+//     if (error) {
+//         console.log('Error sending message: ', error);
+//     } else if (response.body.error) {
+//         console.log('Error: ', response.body.error);
+//     }
+//   });
+// }
+
+
+/* GET query from API.ai */
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat'
+  });
+
+  apiai.on('response', function(response) {
+    console.log(response)
+    let aiText = response.result.fulfillment.speech;
+
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: aiText}
+      }
+    }, function (error, response) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  });
+
+  apiai.on('error', function(error) {
+    console.log(error);
+  });
+
+  apiai.end();
+}
+
+/* Webhook for API.ai to get response from the 3rd party API */
+app.post('/ai', (req, res) => {
+  console.log('*** Webhook for api.ai query ***');
+  console.log(req.body.result);
+
+  if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        console.log(json);
+        let tempF = ~~(json.main.temp * 9/5 - 459.67);
+        let tempC = ~~(json.main.temp - 273.15);
+        let msg = 'The current condition in ' + json.name + ' is ' + json.weather[0].description + ' and the temperature is ' + tempF + ' ℉ (' +tempC+ ' ℃).'
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'
+        });
+      } else {
+        let errorMessage = 'I failed to look up the city name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    })
+  }
+
+});
